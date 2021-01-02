9907df28d236d50d3da0827fbaecbc7b5c5ef530
REIGN12 <3180105323@zju.edu.cn>
2021-01-02 15:30:28 +0800

    Fix a bug in while loop.Use a more general way to parse the string.

--- parser.cc
+++ parser.cc
@@ -56,19 +56,19 @@ class ExprNode
         };
 };
 
-
-
 ExprNode* func_parser(string func_s)
 {
+    /* preprocess start */
     // 利用 regex 做预处理，使得表达式没有空白字符
     regex re_bl(R"(\s+)");
     func_s = regex_replace(func_s,re_bl,"");
-
-    // 利用 regex 做字符串分割, 分为 terms 和 ops 两部分
+    // 利用 regex 将表达式中的 operator 和 term 分开
+    // 返回 exprs (vec of string)
     regex re_op(R"([\+\*\-\/\^])");// using raw string
+    func_s = regex_replace(func_s,re_op," $0 ");
     auto end = sregex_token_iterator();
-    vector<string> terms(sregex_token_iterator(func_s.begin(),func_s.end(),re_op,-1),end);
-    vector<string> ops(sregex_token_iterator(func_s.begin(),func_s.end(),re_op,0),end);
+    vector<string> exprs(sregex_token_iterator(func_s.begin(),func_s.end(),re_bl,-1),end);
+    /* preprocess end */
 
     // 利用 map 表示运算符的优先级
     map<string,int> pri 
@@ -80,29 +80,32 @@ ExprNode* func_parser(string func_s)
     vector<string> sop;
     vector<ExprNode*> sexpr;
     int sexpr_top = 0;
-    
-    sexpr.push_back("x"==terms[0] ? new ExprNode() : new ExprNode(stod(terms[0])));
-    sexpr_top++;
-    for(size_t i = 0; i<ops.size();++i)
+    for(auto&& s : exprs)
     {
-        if(!sop.empty())
+        if(regex_match(s,re_op))
         {
-            while(pri[sop.back()] > pri[ops[i]])
+            while(!sop.empty() && pri[sop.back()] > pri[s])
             {
+                // 对于双目运算符
                 sexpr_top-=2;
                 sexpr.insert(sexpr.begin()+sexpr_top,
-                new ExprNode{sop.back(), sexpr[sexpr_top],sexpr[sexpr_top+1]});
+                new ExprNode{sop.back(),sexpr[sexpr_top],sexpr[sexpr_top+1]});
                 sexpr_top++;
                 sop.pop_back();
             }
+            sop.push_back(s);
         }
-        sop.push_back(ops[i]);
-        sexpr.insert(sexpr.begin()+sexpr_top,
-        "x"==terms[i+1] ? new ExprNode() : new ExprNode(stod(terms[i+1])));
-        sexpr_top++;
+        else 
+        {
+            sexpr.insert(sexpr.begin()+sexpr_top,
+            "x"==s? new ExprNode() : new ExprNode(stod(s)));
+            sexpr_top++;
+        }
     }
+    
     while(!sop.empty())
     {
+        // 对于双目运算符
         sexpr_top -= 2;
         sexpr.insert(sexpr.begin()+sexpr_top,
         new ExprNode{sop.back(),sexpr[sexpr_top],sexpr[sexpr_top+1]});
@@ -117,11 +120,8 @@ int main(int argc,char* argv[])
 {
     ExprNode* p = func_parser(string(argv[1]));
     double x = stod(argv[2]);
-    double t = (*p)(x);
-    cout<<t<<"\n";
-   //ExprNode* p = func_parser("1+2-3*5/2");
-   //double t = (*p)(200.);
 
-
+    cout<<(*p)(x)<<"\n";
+  
     return 0;
 }
